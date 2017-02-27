40b75d9ac0f8f0a96c781d21a7474934ab8ca73c
Kirby Banman <kirby.banman@gmail.com>
2017-02-26 23:29:43 -0700

    Write language pruner

---
+++ remove_non_english.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import shutil
+from collections import Counter
+
+import gutenparse
+
+
+if __name__ == "__main__":
+  dry_run = False
+  if sys.argv[-1] == "--dry":
+    dry_run = True
+    sys.argv.pop()
+
+  all_non_english_languages = Counter()
+  removed = []
+
+  def remove_if_not_english(meta_dict, meta_file_name):
+    language = meta_dict["language"].upper()
+    print language
+    if language != "EN":
+      book_directory = os.path.dirname(os.path.realpath(meta_file_name))
+      all_non_english_languages.update([language])
+
+      print "Removing " + language + " " + book_directory
+
+      if not dry_run:
+          shutil.rmtree(book_directory)
+
+      removed.append(book_directory)
+
+  gutenparse.enumerate_parsed(sys.argv[1:], remove_if_not_english)
+
+  print "\nNon-English languages encountered during removal:"
+  for countee, count in all_non_english_languages.most_common():
+    print countee + " (" + str(count) + ")"
+
+  print "\nRemoved " + str(len(removed)) + " books."
